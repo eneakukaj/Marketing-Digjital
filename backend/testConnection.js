43b4aa75d43d95227cd376c81752bc5151370f90
eneakukaj <ek73805@ubt-uni.net>
2026-04-09 23:03:59 +0200

    instalimi i v6 te prismes dhe file testimi

--- backend/testConnection.js
+++ backend/testConnection.js
@@ -1,22 +1,11 @@
-import { PrismaClient } from '@prisma/client'
-
-// Mos shto asgjë brenda kllapave ()
-const prisma = new PrismaClient()
+import prisma from "./database/db.js";
 
 async function test() {
   try {
-    // Kjo tenton të bëjë një query të thjeshtë për të parë nëse punon
     await prisma.$connect()
-    console.log("------------------------------------------")
-    console.log("✅ SUKSES: Prisma u lidh me MySQL!")
-    console.log("------------------------------------------")
-    
-    // Opsionale: provo të marrësh diçka nga një tabelë (p.sh. User)
-    // const users = await prisma.user.findMany()
-    // console.log("Të dhënat:", users)
-
+    console.log("Prisma u lidh me MySQL")
   } catch (error) {
-    console.error("❌ GABIM GJATË LIDHJES:", error.message)
+    console.error("GABIM", error.message)
   } finally {
     await prisma.$disconnect()
   }
