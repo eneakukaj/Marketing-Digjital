3402cceb81626219638eeae637d1ec7ca15fb966
eneakukaj <ek73805@ubt-uni.net>
2026-04-22 20:43:09 +0200

    inicializimi i react dhe instalimi/konfigurimi i tailwind per css

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('user');
+    if (savedUser) setUser(JSON.parse(savedUser));
+  }, []);
+
+  const login = (userData) => {
+    setUser(userData.user);
+    localStorage.setItem('accessToken', userData.accessToken);
+    localStorage.setItem('user', JSON.stringify(userData.user));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.clear();
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,10 @@
+import api from '../api/axios';
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await api.post('/auth/login', { email, password });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || 'Interruption';
+  }
+};
