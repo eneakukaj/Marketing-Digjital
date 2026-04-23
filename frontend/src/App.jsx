e78d4a25b8d8461c054fff68388a40369afe6348
Vesa Beqa <vb73040@ubt-uni.net>
2026-04-23 22:13:36 +0200

    Rregullimi i navigimit dhe lidhja e faqeve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,55 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import { useContext } from 'react'
+
+import { AuthContext } from './context/AuthContext'
+
+ 
+
 function App() {
+
+  const { user } = useContext(AuthContext);
+
+ 
+
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white uppercase shadow-lg">
-        Tailwind po punon!
-      </h1>
-    </div>
+
+    <Routes>
+
+      {/* Faqja kryesore - nëse s'je i loguar të dërgon te Login */}
+
+      <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
+
+     
+
+      <Route path="/login" element={
+
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+
+          <h1 className="text-2xl font-bold">Këtu do të jetë Faqja e Login-it</h1>
+
+        </div>
+
+      } />
+
+ 
+
+      <Route path="/dashboard" element={
+
+        <div className="p-10">
+
+          <h1 className="text-3xl font-bold text-blue-600">Mirësevini në Dashboard!</h1>
+
+        </div>
+
+      } />
+
+    </Routes>
+
   )
+
 }
+
+ 
 
 export default App
